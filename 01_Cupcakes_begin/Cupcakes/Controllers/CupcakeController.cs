eede5074ff545c1134577bc87ef954a45b226dfe
irinascurtu <scurtuirina@gmail.com>
2020-03-21 09:22:55 +0200

    upgraded the proj

--- 01_Cupcakes_begin/Cupcakes/Controllers/CupcakeController.cs
+++ 01_Cupcakes_begin/Cupcakes/Controllers/CupcakeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Cupcakes.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cupcakes.Controllers
--- 01_Cupcakes_begin/Cupcakes/Startup.cs
+++ 01_Cupcakes_begin/Cupcakes/Startup.cs
@@ -5,28 +5,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cupcakes
 {
-    public class Startup
+	public class Startup
     {
+        private IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+           
+            services.AddHttpContextAccessor();
+            services.AddControllersWithViews();
+           
         }
-
         public void Configure(IApplicationBuilder app)
         {
+          
             app.UseStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "CupcakeRoute",
-                    template: "{controller}/{action}/{id?}",
-                    defaults: new { controller = "Cupcake", action = "Index" },
-                    constraints: new { id = "[0-9]+" });
+            app.UseRouting();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Cupcake}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
